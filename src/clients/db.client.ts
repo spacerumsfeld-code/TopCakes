6a60d9418622f082d16cb2279605b79cb475842d
Nick Papadakis <nickfin2014@gmail.com>
2024-11-04 17:47:46 -0500

    for reals

--- src/clients/db.client.ts
+++ src/clients/db.client.ts
@@ -2,10 +2,6 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import { neon } from '@neondatabase/serverless'
 import { Resource } from 'sst'
 
-const sql = neon(
-    process.env.NEXT_PUBLIC_IS_LOCAL!
-        ? process.env.DATABASE_URL!
-        : Resource.DatabaseUrl.value,
-)
+const sql = neon(Resource.DatabaseUrl.value)
 
 export const db = drizzle(sql)
